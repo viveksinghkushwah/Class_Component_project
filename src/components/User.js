e1ef618f57d4f1bb515e04050b2a7da7e4d4cf48
Kushwah <viveksingh.kushwah@solera.com>
2022-11-07 12:11:29 +0530

    a

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,19 @@
 import classes from './User.module.css';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+import React, { Component } from 'react'
 
+class User extends Component {
+  render() {
+      return <li className={classes.user}>{this.props.name}</li>; 
+  }
+}
 export default User;
+
+
+
+// import classes from './User.module.css';
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
+
+// export default User;
